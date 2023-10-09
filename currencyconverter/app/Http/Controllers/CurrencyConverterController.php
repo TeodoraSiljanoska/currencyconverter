256af9bbd10c350ddfeaccfb6f0a747841a2e176
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-10-10 01:55:45 +0200

    Exception Handling, Amount Validation and Logging added in the CurrencyConverter Controller

--- currencyconverter/app/Http/Controllers/CurrencyConverterController.php
+++ currencyconverter/app/Http/Controllers/CurrencyConverterController.php
@@ -3,47 +3,83 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http; 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
 
 class CurrencyConverterController extends Controller
 {
     public function convert(Request $request)
     {
-        $amount = $request->input('amount');
-        $baseCurrency = 'EUR';
-        $targetCurrencies = ['USD', 'AUD', 'CAD', 'CHF', 'MKD', 'JPY'];
+        try {
+            $amount = $request->input('amount');
 
-        $exchangeRates = $this->getExchangeRates($baseCurrency, $targetCurrencies);
-
-        $convertedValues = [];
-        foreach ($targetCurrencies as $currency) {
-            if (isset($exchangeRates[$currency])) {
-                $convertedValues[$currency] = $amount * $exchangeRates[$currency];
+            // Validate the amount
+            if (!is_numeric($amount) || $amount <= 0) {
+                return response()->json([
+                    'success' => false,
+                    'error' => 'Invalid amount. Please enter valid amount for conversion.',
+                ]);
             }
-        }
 
-        return response()->json([
-            'success' => true,
-            'data' => $convertedValues,
-        ]);
+            $baseCurrency = 'EUR';
+            $targetCurrencies = ['USD', 'AUD', 'CAD', 'CHF', 'MKD', 'JPY'];
+
+            $exchangeRates = $this->getExchangeRates($baseCurrency, $targetCurrencies);
+
+            if (empty($exchangeRates)) {
+                // Handle empty exchange rates data
+                return response()->json([
+                    'success' => false,
+                    'error' => 'Failed to fetch exchange rates data.',
+                ]);
+            }
+
+            $convertedValues = [];
+            foreach ($targetCurrencies as $currency) {
+                if (isset($exchangeRates[$currency])) {
+                    $convertedValues[$currency] = $amount * $exchangeRates[$currency];
+                }
+            }
+
+            return response()->json([
+                'success' => true,
+                'data' => $convertedValues,
+            ]);
+        } catch (\Exception $e) {
+            // Handle any general exceptions
+            Log::error('Error in CurrencyConverterController: ' . $e->getMessage());
+            return response()->json([
+                'success' => false,
+                'error' => 'An error occurred while processing the request.',
+            ]);
+        }
     }
 
     private function getExchangeRates($baseCurrency, $targetCurrencies)
     {
-        $client = new Client();
-        $apiKey = '994289dba30a7f4ac48d182770464a8d'; // My API key
+        try {
+            $client = new Client();
+            $apiKey = '994289dba30a7f4ac48d182770464a8d'; // My API key
 
-        $response = $client->get("http://api.exchangeratesapi.io/v1/latest?access_key=$apiKey&format=1");
+            $response = $client->get("http://api.exchangeratesapi.io/v1/latest?access_key=$apiKey&format=1");
 
-        $data = json_decode($response->getBody(), true);
+            $data = json_decode($response->getBody(), true);
 
-        if (isset($data['rates'])) {
-        return $data['rates'];
+            if (isset($data['rates'])) {
+                return $data['rates'];
+            } else {
+                // Handle missing rates data
+                return [];
+            }
+        } catch (RequestException $e) {
+            // Handle Guzzle HTTP request exception
+            Log::error('Error in CurrencyConverterController (HTTP): ' . $e->getMessage());
+            return [];
+        } catch (\Exception $e) {
+            // Handle any other exceptions
+            Log::error('Error in CurrencyConverterController: ' . $e->getMessage());
+            return [];
         }
-
-        return [];
     }
 }
-    
-    
